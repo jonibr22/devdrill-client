469d7261970783f77b8c54333711f795ad381cc2
Jonathan Ibrahim <jonathanibrahim22@yahoo.com>
2020-09-27 16:40:01 +0700

    fix & add some methods in json-server db

--- server/server.ts
+++ server/server.ts
@@ -1,41 +1,49 @@
+import { exception } from "console";
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('server/db.json');
 const middlewares = jsonServer.defaults();
-const db = require('./db.json');
+const _ = require('lodash')
 const fs = require('fs');
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
 //LOGIN USER API
-server.post('/login', (req, res, next) => { 
-  const users = readUsers(); //get all users
+server.post('/login', (req, res) => { 
+  const db = router.db;
+  const users = all(db,'users'); //get all users
   const user = users.filter(
     u => u.email === req.body.email && u.password === req.body.password
-  )[0]; //filter users by email & password
-
-  if (user) { //check if user exists
+  ); //filter users by email & password
+  if (any(user)) { //check if user exists
     res.send({ ...formatUser(user), token: generateToken(user) });
-  } 
+  }
   else {
     res.status(401).send('Incorrect email or password');
   }
 });
 //REGISTER USER API
 server.post('/register', (req, res) => {
-  const users = readUsers(); //get all users
-  const user = users.filter(u => u.email === req.body.email)[0]; //filter users by email 
+  const db = router.db;
+  const users = all(db,'users'); //get all users
+  const user = users.filter(u => u.email === req.body.email); //filter users by email 
   //check if email does not exists
-  if (user === undefined || user === null) {
-    res.send({...formatUser(req.body),token: generateToken(req.body)
-    });
-    db.users.push(req.body); //add to db.json
+  if (!any(user)) {
+    insert(db,'users',{
+      email: req.body.email,
+      password: req.body.password,
+      phone: req.body.phone,
+      name: req.body.name
+    }); //add to db.json
+    res.send({...formatUser(req.body),token: generateToken(req.body)});
   } 
   else {
     res.status(500).send('User already exists');
   }
 });
+
 // // 
 // server.use('/users', (req, res, next) => {
 //   if (isAuthorized(req) || req.query.bypassAuth === 'true') {
@@ -45,17 +53,12 @@ server.post('/register', (req, res) => {
 //   }
 // });
 
-server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running');
-});
 
 function formatUser(user) {
   delete user.password;
   user.signin = new Date();
   return user;
 }
-
 function generateToken(user) {
   return "user-token"
 }
@@ -64,8 +67,51 @@ function generateToken(user) {
 //   return req.headers.authorization === 'admin-token' ? true : false;
 // }
 
-function readUsers() {
-  const dbRaw = fs.readFileSync('./server/db.json');  
-  const users = JSON.parse(dbRaw).users
-  return users;
+//INNER CRU
+function all(db, collection){
+  const table = db.get(collection);
+  return table;
 }
+function find(db, collection, id){
+  const table = db.get(collection);
+  return table.find(item => item.id == id);
+}
+function insert(db, collection, body){
+  const table = db.get(collection);
+  const exists = any(table);
+  let latestId = 0;
+  if(exists){
+    latestId = Math.max(...table.map(item=>item.id));
+  }
+  body.id = latestId + 1;
+  table.push(body).write();
+  return body;
+}
+function update(db, collection, body){
+  const table = db.get(collection);
+  const found = table.find(item => item.id == body.id);
+  if(found.value()){
+    table.find(item => item.id == body.id)
+      .assign(_.omit(body, ['id']))
+      .write()
+  }
+}
+////
+
+// read if array has any element 
+// (i made this bcuz every effort i tried to check if db.get array is empty or no is useless)
+// (in other words, this method is the only one which is working)
+function any(table){
+  return table.map(item => 1).find(x => x == 1).value();
+}
+
+// server.post('/dummy', (req, res) => {
+//   const db = router.db;
+//   const e = update(db,'users',req.body);
+//   res.send(e);
+// })
+
+server.use(router);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
